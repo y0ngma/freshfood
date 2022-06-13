a6e8f04016af7a9578070209c6e162f505b05321
y0ngma <takeit2sy@pusan.ac.kr>
2022-06-13 15:24:07 +0900

    추가 데이터확인 부분

--- train_main.py
+++ train_main.py
@@ -12,21 +12,40 @@ import time
 import os
 import copy
 import random
-
 from efficientnet_pytorch import EfficientNet
-model_name = 'efficientnet-b0'  # b5
 
-image_size = EfficientNet.get_image_size(model_name)
-print(image_size)
-model = EfficientNet.from_pretrained(model_name, num_classes=10)
-batch_size  = 128
+class_names = {
+    "0": "apple",
+    "1": "banana",
+    "2": "carrot",
+    "3": "cauliflower",
+    "4": "garlic",
+    "5": "ginger",
+    "6": "grapes",
+    "7": "mango",
+    "8": "pineapple",
+    "9": "watermelon"
+}
+model_name = 'efficientnet-b0'  # b5
+model = EfficientNet.from_pretrained(model_name, num_classes=len(class_names))
+print(EfficientNet.get_image_size(model_name))
+
+# print("fc 제외하고 freeze")
+# for n, p in model.named_parameters():
+#     if '_fc' not in n:
+#         p.requires_grad = False
+# model = torch.nn.parallel.DistributedDataParallel(model)
+
+batch_size  = 32
 random_seed = 555
 random.seed(random_seed)
 torch.manual_seed(random_seed)
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+data_path   = f"{os.path.dirname(PROJECT_DIR)}/dataset/{os.path.basename(PROJECT_DIR)}"
+
 ## make dataset
 from torchvision import transforms, datasets
-data_path = 'president/president_data'  # class 별 폴더로 나누어진걸 확 가져와서 라벨도 달아준다
 president_dataset = datasets.ImageFolder(
                                 data_path,
                                 transforms.Compose([
@@ -59,3 +78,42 @@ dataloaders['test']  = torch.utils.data.DataLoader(datasets['test'],
                                               num_workers=4)
 batch_num['train'], batch_num['valid'], batch_num['test'] = len(dataloaders['train']), len(dataloaders['valid']), len(dataloaders['test'])
 print('batch_size : %d,  tvt : %d / %d / %d' % (batch_size, batch_num['train'], batch_num['valid'], batch_num['test']))
+
+## 데이타 체크
+import torchvision
+def imshowt(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+num_show_img = 5
+class_names  = {"0": "apple","1": "banana","2": "carrot","3": "cauliflower","4": "garlic","5": "ginger","6": "grapes","7": "mango","8": "pineapple","9": "watermelon"}
+
+print(dataloaders['train'])
+print(iter(dataloaders['train']))
+
+# The "freeze_support()" line can be omitted if the program is not going to be frozen to produce an executable.
+
+
+# # train check
+# inputs, classes = next(iter(dataloaders['train']))
+# print(inputs, classes)
+# out = torchvision.utils.make_grid(inputs[:num_show_img])  # batch의 이미지를 오려부친다
+# imshowt(out, title=[class_names[str(int(x))] for x in classes[:num_show_img]])
+
+# # valid check
+# inputs, classes = next(iter(dataloaders['valid']))
+# out = torchvision.utils.make_grid(inputs[:num_show_img])  # batch의 이미지를 오려부친다
+# imshowt(out, title=[class_names[str(int(x))] for x in classes[:num_show_img]])
+
+# # test check
+# inputs, classes = next(iter(dataloaders['test']))
+# out = torchvision.utils.make_grid(inputs[:num_show_img])  # batch의 이미지를 오려부친다
+# imshowt(out, title=[class_names[str(int(x))] for x in classes[:num_show_img]])
